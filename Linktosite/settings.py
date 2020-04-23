77854dcf1e74e69993150acfd62b739b1e2bd19d
Artem Fadeev <artemfap@yandex.ru>
2020-04-23 18:10:56 +0300

    fix10

--- Linktosite/settings.py
+++ Linktosite/settings.py
@@ -111,8 +111,8 @@ if os.getcwd() == '/app':
 
     DEBUG = False
 
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static_in_dev'),
-    )
+    # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    # STATIC_ROOT = 'staticfiles'
+    # STATICFILES_DIRS = (
+    #     os.path.join(BASE_DIR, 'static_in_dev'),
+    # )
