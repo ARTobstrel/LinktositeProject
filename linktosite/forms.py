6ec79ba14106167ab14c6157abe53f47868498e9
Artem Faradey <artemfap@yandex.ru>
2020-06-25 01:26:09 +0300

    fix1

--- linktosite/forms.py
+++ linktosite/forms.py
@@ -2,13 +2,21 @@ from django import forms
 
 from .models import Link
 
+
 class LinkForm(forms.ModelForm):
+
     class Meta:
         model = Link
-        fields = ['category', 'title', 'link', 'image']
+        fields = ('category', 'title', 'link', 'image')
         labels = {
             'category': 'Категория:',
             'title': 'Имя ресурса:',
             'link': 'Ссылка на ресурс:',
             'image': 'Иконка:'
+        }
+        widgets = {
+            'category': forms.Select(attrs={'class': 'form_field__category'}),
+            'title': forms.TextInput(attrs={'class': 'form_field__title'}),
+            'link': forms.TextInput(attrs={'class': 'form_field__link'}),
+            'image': forms.FileInput(attrs={'class': 'form_field__image'}),
         }
